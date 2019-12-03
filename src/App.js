43f2657c8245dc23c3df657e1febd86c21489f94
trodriguez89 <trodriguez89@gmail.com>
2019-12-03 13:15:02 -0800

    mvp wip

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from "axios";
+import UserCard from "./components/UserCard";
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    user: "",
+    followers: []
+  }
+
+  componentDidMount() {
+    axios.get("https://api.github.com/users/trodriguez89")
+      .then(response => {
+        console.log(response.data);
+        this.setState({ user: response.data })
+      })
+      .catch(error => {
+        console.log("Oops, something went wrong!", error)
+      })
+    axios.get("https://api.github.com/users/trodriguez89/followers")
+      .then(response => {
+        console.log(response.data)
+        this.setState({ followers: response.data })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Thomas' Github Info</h1>
+        <p>{this.user.name}</p>
+        <UserCard user={this.user} followers={this.followers} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/UserCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserCard = props => {
+    return (
+            <div>
+                {/* <h2>{props.user.name}</h2> */}
+            </div>
+    );
+}
+
+export default UserCard;
